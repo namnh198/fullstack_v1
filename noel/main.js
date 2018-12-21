57c6b4aa6d3c94031b4eb67480b03ce270535466
tieugum <namptit.dev@gmail.com>
2018-12-22 00:39:43 +0700

    Ahihi

---
+++ noel/main.js
@@ -0,0 +1,56 @@
+$('#christmas_scene').parallax({
+	scalarX: 5,
+	scalarY: 15,
+	invertY: false
+});
+
+$(window).on('load', function(){
+	setParallaxHeight();
+	setElementHeight();
+});
+
+$(window).on('resize', function(){
+	setParallaxHeight();
+	setParallaxWidth();
+	setElementHeight();
+});
+
+function setParallaxHeight() {
+	var height = $(window).height();
+	$('#christmas_scene .layer-photo').css('height' , height);
+}
+
+function setParallaxWidth() {
+	var width = $(window).width();
+	$('#christmas_scene .layer-photo').css('width' , width);
+}
+
+function setElementHeight() {
+	var height = $(window).height();
+
+	if(height <= 400) {
+		var width = height / 2;
+	} 
+	else if (height <= 500) {
+		var width = height / 3.5;
+	}
+	else if (height <= 700) {
+		var width = height / 3;
+	}
+	else if (height <= 800) {
+		var width = height / 2.8;
+	}
+
+	$('#christmas_tree').css('width', width);
+}
+
+$('#countdown').countdown('2018/12/24', function(event) {
+	$(this).html(
+		event.strftime('\
+			<div class="col-3 countdown-globe">%D <span>Days</span></div>\
+			<div class="col-3 countdown-globe">%H <span>Hours</span></div>\
+			<div class="col-3 countdown-globe">%M <span>Minutes</span></div>\
+			<div class="col-3 countdown-globe">%S <span>Seconds</span></div>\
+		')
+	)
+});
